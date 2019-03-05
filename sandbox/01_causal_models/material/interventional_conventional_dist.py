e02cb26f6bd4a62e0eb4efa236ea34eccc550dc3
luward <luis.wardenbach@uni-bonn.de>
2019-03-05 16:45:34 +0100

    updated the conditional distribution

--- sandbox/01_causal_models/material/interventional_conventional_dist.py
+++ sandbox/01_causal_models/material/interventional_conventional_dist.py
@@ -15,6 +15,7 @@ plt.plot(bins, 1/(sd * np.sqrt(2 * np.pi)) * np.exp(- (bins - mean)**2 / (2 * sd
 ax.set_xlim(-3, 3)
 ax.set_ylim(0, 0.55)
 ax.set_xlabel('X')
+ax.set_title('Interventional distribution of X|do(Y=3)')
 
 plt.savefig('interventional_distribution.png')
 
@@ -22,16 +23,18 @@ plt.savefig('interventional_distribution.png')
 # Conditional distribution
 
 # Plot graph
-mean = [0, 0]
-rho = -6
-cov = [[1, rho], [rho, 37]]
+sigma1 = 1
+sigma2 = 6.0827625303  # sqrt(37)
+rho = -0.98639392383  # -6/sigma2
+mean = (sigma1/sigma2) * rho * 3
+sd = 1 - rho ** 2
 fig, ax = plt.subplots()
-x, y = np.random.multivariate_normal(mean, cov, 1000).T
-ax.plot(x, y, 'x')
+x = np.random.normal(mean, sd, 1000)
+count, bins, ignored = plt.hist(x, 30, density=True)
+plt.plot(bins, 1/(sd * np.sqrt(2 * np.pi)) * np.exp(- (bins - mean)**2 / (2 * sd**2)), linewidth=2, color='r')
 
 # Set labels, limits and title
-ax.set_xlim(-4, 4)
 ax.set_xlabel('X')
-ax.set_ylabel('Y')
+ax.set_title('Conditional distribution of X|Y=3')
 
 plt.savefig('conditional_distribution.png')
